2a425a532aea4c6c255e7846d05be9765a65c569
zachariahj <ZachariahJ@CITRIX-ZACHJEY.citrite.net>
2015-04-09 10:19:01 -0400

    serve submit page, receive submits

--- server.go
+++ server.go
@@ -5,12 +5,16 @@ import "net/http"
 //import "net/url"
 
 func handle(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(r.URL.String());
+	if (r.Method == "GET") {
+		http.ServeFile(w, r, "static/submit.html")
+	} else {
+		fmt.Println("post")
+	}
 }
 
 func main() {
 	fmt.Printf("Hello world\n")
-	appPrefix := "aggregatedeck"
+	appPrefix := "metadeck"
 	http.HandleFunc(fmt.Sprintf("/%s/", appPrefix), handle)	
 	staticPath := fmt.Sprintf("/%s/static/", appPrefix)
 	http.Handle(staticPath, http.StripPrefix(staticPath, http.FileServer(http.Dir("static"))))
