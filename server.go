cff32a99ec669ecdc513c22954b17209f99a25d5
zachariahj <ZachariahJ@CITRIX-ZACHJEY.citrite.net>
2015-04-08 20:24:42 -0400

    serve static file

--- server.go
+++ server.go
@@ -2,13 +2,17 @@ package main
 
 import "fmt"
 import "net/http"
+//import "net/url"
 
 func handle(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("req");
+	fmt.Println(r.URL.String());
 }
 
 func main() {
 	fmt.Printf("Hello world\n")
-	http.HandleFunc("/aggregatedeck", handle)
+	appPrefix := "aggregatedeck"
+	http.HandleFunc(fmt.Sprintf("/%s/", appPrefix), handle)	
+	staticPath := fmt.Sprintf("/%s/static/", appPrefix)
+	http.Handle(staticPath, http.StripPrefix(staticPath, http.FileServer(http.Dir("static"))))
 	http.ListenAndServe(":8981", nil)
 }
