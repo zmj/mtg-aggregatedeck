8597d6f2567d1dcfd6f8f7002b9c074ce7d3e8b0
zachariahj <zachariahj@CITRIX-ZACHJEY.citrite.net>
2015-04-15 10:03:14 -0400

    tests for parsing

---
+++ parse_test.go
@@ -0,0 +1,56 @@
+package main 
+
+import (
+	"testing"
+	"os"
+	"errors"
+)
+
+func TestScgDecklist(t *testing.T) {
+	filename := "scg_grag_1.txt"
+	_, err := ParseDecklist(filename)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestWotcDecklist(t *testing.T) {
+	filename := "wotc_ptdtk_abzag_1.txt"
+	_, err := ParseDecklist(filename)
+	if err != nil {
+		t.Error(err)
+	}	
+}
+
+func TestMwsDecklist(t *testing.T) {
+	filename := "mws_modabz_1.mwDeck"
+	_, err := ParseDecklist(filename)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func ParseDecklist(filename string) (*Deck, error) {
+	file, err := os.Open("sampledata/"+filename)
+	if err != nil {
+		return nil, err
+	}
+	deck, err := NewDeck(file)
+	if err != nil {
+		return nil, err
+	}
+	if CountCards(deck.maindeck)<60 {
+		return nil, errors.New("Maindeck less than 60 cards")
+	} 
+	if CountCards(deck.sideboard)>15 {
+		return nil, errors.New("Sideboard more than 15 cards")
+	}
+	return deck, nil
+}
+
+func CountCards(deck []*Card) (count int) {
+	for _,c := range deck {
+		count += c.quantity
+	}
+	return count
+}
